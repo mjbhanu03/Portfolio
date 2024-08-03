528c22e072a784566e0f70680b5bee1c3bbf38e4
Mange Jay <jaymange263@gmail.com>
2024-08-03 23:41:09 +0530

    Portfolio

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    // Html File
+    include('./Html/html-start.html');
+
+    // After nav 
+    include('./Home Content/content_1.html');
+
+    // After main content
+    include('./Home Content/content_2.html');
+
+    // Contact ME content
+    include('./Home Content/content_3.html');
+
+    include('./Html/html-end.html');
+?>
